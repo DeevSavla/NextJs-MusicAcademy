f2398de7c2e23784bba745cf99fea9b59c02392e
Deev Savla <143065962+DeevSavla@users.noreply.github.com>
2024-07-08 20:47:15 +0530

    added featured courses section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -20,7 +20,7 @@ function HeroSection() {
                     Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true potential.
                 </p>
                 <div className='mt-9'>
-                    <Link href='/courses'>
+                    <Link href={'/courses'}>
                         <Button
                             borderRadius="1.75rem"
                             className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
